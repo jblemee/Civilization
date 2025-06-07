5245b61929d9d487c016667f87f4433eb38af7a3
Jean-Baptiste Lemée <git@lemee.co>
2025-06-06 18:02:16 -1000

    Minecraft 1.21.5

--- src/main/java/pf/maana/civilization/Config.java
+++ src/main/java/pf/maana/civilization/Config.java
@@ -1,11 +1,11 @@
 package pf.maana.civilization;
 
-import net.neoforged.fml.common.EventBusSubscriber;
+//import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.common.ModConfigSpec;
 
 // An example config class. This is not required, but it's a good idea to have one to keep your config organized.
 // Demonstrates how to use Neo's config APIs
-@EventBusSubscriber(modid = Civilization.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
+//@EventBusSubscriber(modid = Civilization.MOD_ID, bus = EventBusSubscriber.Bus.MOD)
 public class Config
 {
     private static final ModConfigSpec.Builder BUILDER = new ModConfigSpec.Builder();
